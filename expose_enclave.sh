2c90d3cfa666d871f2a6222aae057ca6e6906252
VaughnMYA <vaughn@mysoft.co.za>
2025-08-20 13:51:07 +0200

    fix expose_enclave

--- expose_enclave.sh
+++ expose_enclave.sh
@@ -2,18 +2,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #!/bin/bash
 
-# Gets the encalve id and CID
+# Gets the enclave id and CID
 # expects there to be only one enclave running
 ENCLAVE_ID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
 ENCLAVE_CID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveCID")
 
 sleep 5
 # Secrets-block
-SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:227314482053:secret:dfusion-sui-secret-cnOcUh --region us-east-1 | jq -r .SecretString)
-echo "$SECRET_JSON" > secrets.json
+SECRET_JSON=$(aws secretsmanager get-secret-value \
   --secret-id arn:aws:secretsmanager:us-east-1:227314482053:secret:dfusion-sui-secret-cnOcUh \
   --region us-east-1 \
   --profile vaughn-nitro | jq -r .SecretString)
+echo "$SECRET_JSON" > secrets.json
 
 cat secrets.json | socat - VSOCK-CONNECT:$ENCLAVE_CID:7777
 socat TCP4-LISTEN:3000,reuseaddr,fork VSOCK-CONNECT:$ENCLAVE_CID:3000 &
