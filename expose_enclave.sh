3990b96657b9c96e78b1e6ac84014523e1703ce2
VaughnMYA <vaughn@mysoft.co.za>
2025-09-08 12:49:49 +0200

    nautilus configure

--- expose_enclave.sh
+++ expose_enclave.sh
@@ -9,7 +9,7 @@ ENCLAVE_CID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveCID")
 
 sleep 5
 # Secrets-block
-SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:227314482053:secret:sui-nautilus-Wn9HXs --region us-east-1 | jq -r .SecretString)
+SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:227314482053:secret:sui-tee-qf2z2v --region us-east-1 | jq -r .SecretString)
 echo "$SECRET_JSON" > secrets.json
 
 cat secrets.json | socat - VSOCK-CONNECT:$ENCLAVE_CID:7777
